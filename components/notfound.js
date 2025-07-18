589df1c8f43b299b9afa2c76daa40e05a5b175a1
Mahesh <24f1002414@ds.study.iitm.ac.in>
2025-07-18 10:33:41 +0530

    Modified Paths and added some newones

---
+++ components/notfound.js
@@ -0,0 +1,9 @@
+const NotFound = {
+    template: `
+    <div>
+        <h4> Page Not found </h4>
+    </div>
+    `
+}
+
+export default NotFound
--- script.js
+++ script.js
@@ -2,22 +2,29 @@ import Home from './components/Home.js'
 import Profile from './components/Profile.js'
 import Dashboard from './components/Dashboard.js'
 import Payments from './components/Payment.js'
+import NotFound from './components/notfound.js'
 
 const routes = [
-{path: '/home', component: Home},
+{path: '/home', name:"home", component: Home},
 { 
     path: '/profile/:user',
     component: Profile,
+    name: 'profile',
     children:[
       {  path: "dashboard",
+        name: 'dash',
         component: Dashboard
       },
       {
         path: "payments",
+        name: 'pay',
         component: Payments
       }
     ]
-} 
+},
+  {
+    path: '*',component: NotFound
+  }
 ]
 
 const router = new VueRouter({
@@ -27,7 +34,25 @@ const router = new VueRouter({
 
 const app = new Vue({
     el: "#app",
-    router: router
+    router: router,
+    data:{
+      user: 'user123'
+    },
+    methods:{
+      dash:function(){
+          this.$router.push({name:'dash',params:{user:this.user}})
+
+      },
+      pay: function(){
+          this.$router.push({name:'pay',params:{user: this.user},query:{type:"admin"}})
+      },
+      goHome: function(){
+        this.$router.push({path:'/home',query:{team:'titan'}})
+      },
+      goback:function(){
+        this.$router.go(-1)
+      }
+    }
 })
 
 
